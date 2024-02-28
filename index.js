72beee3eed47ad3d7fb120a6149425a78b77e753
Abdul Aleem <engineerabdulaleem@gmail.com>
2024-02-28 14:32:57 +0500

    update firebase

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ import App from "./src";
 import { name as appName } from "./app.json";
 registerRootComponent(App);
 const firebaseConfig = {
-  apiKey: "AIzaSyBNgp2owPCTFdoonCCfEIA3c-mdTP52bXs",
-  authDomain: "eidcarosse-7d282.firebaseapp.com",
-  databaseURL:
-    "https://eidcarosse-7d282-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "eidcarosse-7d282",
-  storageBucket: "eidcarosse-7d282.appspot.com",
-  messagingSenderId: "232832919856",
-  appId: "1:232832919856:web:5be06308749ca284f0c0f5",
-  measurementId: "G-KW6JM6FR4Q",
+  apiKey: "AIzaSyBfTQ7IigRYXnp0DExoeutidqdN2xfljM0",
+  authDomain: "dharti-a72bd.firebaseapp.com",
+  databaseURL: "https://dharti-a72bd-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "dharti-a72bd",
+  storageBucket: "dharti-a72bd.appspot.com",
+  messagingSenderId: "606808017425",
+  appId: "1:606808017425:web:eb841f4c5e173f55dfffb0",
+  measurementId: "G-E842D275X3"
 };
 
 import { getApps, initializeApp } from "firebase/app";
