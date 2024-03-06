231510c93ce7d524ef4e692c02d9c9d99cb9ba7e
Abdul Aleem <engineerabdulaleem@gmail.com>
2024-03-06 18:06:33 +0500

    add firtease otp

--- src/asset/images/index.js
+++ src/asset/images/index.js
@@ -1,18 +1,10 @@
 const Icons = {
-    login:require('./log.png'),
-    logName:require('./eidcarose.png'),
-    mainLogo:require('./mainLog.png'),
     accountbg:require('./account_bg.png'),
-    htsf1:require('./htsf1.png'),
-    htsf2:require('./htsf2.png'),
-    htsf3:require('./htsf3.png'),
     tnc:require('./tnc_bg.png'),
     pp:require('./pp_bg.png'),
     sellnow:require('./sellnow.png'),
-    dStore:require('./repairnow.png'),
     loder:require('./newloder.gif'),
     empty:require('./empty.png'),
-    emptyChat:require('./chatNotFound.png'),
     bglogo:require('./product_info_icon.png'),
     iconLogo:require('./icon.jpeg')
   };
