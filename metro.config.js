47fcb116580526215220427dee2053d6fb014e6f
Abdul Aleem <engineerabdulaleem@gmail.com>
2024-03-06 11:49:39 +0500

    update expo eas

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,7 @@
-module.exports = {
-    resolver: {
-      useWatchman: false,
-    },
-  };
+const { getDefaultConfig } = require("@expo/metro-config");
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.assetExts.push("cjs");
+
+module.exports = defaultConfig;
